3ac0a40ccfa08a42b138670007e5098bfa2015b1
Haiming Xu <haimingxu@Haimings-MacBook-Pro.local>
2020-06-10 14:34:52 -0400

    clean

---
+++ GoogleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleProject
+//
+//  Created by Haiming Xu on 6/10/20.
+//  Copyright © 2020 Haiming Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
